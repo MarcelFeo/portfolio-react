f326239b76ea7279ed8abef4ed36611a985ed8a5
MarcelFeo <marcelfeo29@gmail.com>
2022-05-11 14:27:24 -0300

    instalando react icons

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Header from '../Header';
+
+import { Container, Text } from './styles';
 
 export default function About() {
+  const data = new Date();
+  const year = data.getFullYear();
+
   return (
-    <div>about</div>
+    <>
+      <Header />
+
+      <Container>
+        <Text>
+          Meu nome é Marcel Féo, tenho {year - 2004} anos e atualmente sou autodidata. Recentemente, tenho me apaixonado cada vez mais pela área de Desenvolvimento Web então venho participando de muitos eventos e estudando as tecnologias mais importantes para o cargo de Front-End.
+        </Text>
+      </Container>
+    </>
   )
 }
--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    
+    margin-top: -5rem;
+    padding: 2rem;
+`;
 
+export const Text = styled.p`
+    font-size: 1rem;
+    text-align: center;
+    text-transform: uppercase;
+
+    @media (min-width: 811px) {
+        margin-right: 10rem;
+        margin-left: 10rem;
+        font-size: 1.5rem;
+    }
 `;
