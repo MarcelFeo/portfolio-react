8ae0626cf9e6bbc219d7c3e45b4d19d2c736f246
MarcelFeo <marcelfeo29@gmail.com>
2022-05-12 14:35:15 -0300

    finalizando a estilização do formulário

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../Header';
 
-import { Container, Form, Input, TextArea } from './styles';
+import { Container, Form, Input, TextArea, InputButton, Title } from './styles';
 
 export default function Contact() {
   return (
@@ -9,6 +9,8 @@ export default function Contact() {
         <Header />
 
         <Container>
+          <Title>Entre em <span>contato</span> comigo! 👋</Title>
+
           <Form id="contactform" action="https://formsubmit.io/send/marcelfeo29@gmail.com" method="POST">
             <Input name="name" type="text" id="name" placeholder="Name" />
 
@@ -16,7 +18,7 @@ export default function Contact() {
 
             <TextArea name="comment" id="comment" rows="3" placeholder="Message" style={{resize: 'none'}}></TextArea>
 
-            <Input value="Submit" type="submit" />
+            <InputButton value="Submit" type="submit" />
           </Form>
         </Container>
     </>
--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -5,16 +5,99 @@ export const Container = styled.div`
     justify-content: center;
     align-items: center;
     flex-direction: column;
+
+    animation: slide-up .6s cubic-bezier(0.4, 0, 0.2, 1) 0ms backwards;
+
+    @keyframes slide-up {
+        0% {
+            transform: translateY(100%);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    };
+`;
+
+export const Title = styled.h1`
+    font-size: 1.25rem;
+    margin-top: -6rem;
+    text-align: center;
+    text-transform: uppercase;
+
+    > span {
+        color: var(--pink);
+    }
+
+    @media (min-width: 811px) {
+        font-size: 2.5rem;
+    }
 `;
 
 export const Form = styled.form`
-    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    margin-top: 4rem;
+
+    @media (min-width: 811px) {
+        margin-top: 5rem;
+    }
 `;
 
 export const Input = styled.input`
+    padding: 0.5rem;
+    margin-bottom: 1.5rem;
+    border-radius: 6px;
+    border: none;
 
+    box-shadow: 0 0 0 0;
+    outline: 0;
+
+    &:focus {
+        border: 3px solid var(--pink);
+    }
+
+    @media (min-width: 811px) {
+        width: 35rem;
+        padding: 0.75rem;
+    }
 `;
 
 export const TextArea = styled.textarea`
+    padding: 0.5rem;
+    margin-bottom: 1.5rem;
+    border-radius: 6px;
+    border: none;
 
+    box-shadow: 0 0 0 0;
+    outline: 0;
+
+    &:focus {
+        border: 3px solid var(--pink);
+    }
+
+    @media (min-width: 811px) {
+        width: 35rem;
+        padding: 0.75rem;
+    }
+`;
+
+export const InputButton = styled.input`
+    padding: 0.5rem;
+    border-radius: 6px;
+    border: none;
+    background-color: var(--pink);
+    color: var(--black);
+    
+    &:hover {
+        cursor: pointer;
+        filter: brightness(90%);
+    }
+
+    @media (min-width: 811px) {
+        width: 35rem;
+        padding: 0.75rem;
+    }
 `;
